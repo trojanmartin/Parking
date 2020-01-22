d5756fa5d9505f7e69ef03f74e4f47e0e9629e4e
Martin <martin.trojan.372@gmail.com>
2020-01-22 19:17:11 +0100

    trying to resolve issu with swagger ui

--- Source/Api/Parking.Api/Startup.cs
+++ Source/Api/Parking.Api/Startup.cs
@@ -129,7 +129,7 @@ namespace Parking.Api
             // specifying the Swagger JSON endpoint.
             app.UseSwaggerUI(c =>
             {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.SwaggerEndpoint("../swagger/v1/swagger.json", "My API V1");
                 c.RoutePrefix = string.Empty;
                 
             });
